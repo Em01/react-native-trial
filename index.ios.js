6e5df1b60cf522040620900199019b0bdc130134
Em01 <emma_williams1@hotmail.co.uk>
2017-01-28 19:32:10 +0000

    typo fix

--- index.ios.js
+++ index.ios.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Text, AppRegistry } from 'react-native'
 
-const App = () => {
+const App = () => (
   <Text>Some Text</Text>
-};
+)
 
 AppRegistry.registerComponent('albums', () => App)
